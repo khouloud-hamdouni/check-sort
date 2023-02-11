0d2d90c34cb7e62543436711b654c444ec517cc9
khouloud-hamdouni <khouloudhamdouni20@gmail.com>
2023-02-11 14:52:27 +0100

    first commit

---
+++ sort.js
@@ -0,0 +1,27 @@
+
+    let i, j;
+    
+     let arr=[12,11,13,5,6];
+     let n =arr.length;
+     
+     
+
+
+    for (i = 1; i < n; i++)
+    { 
+        key=arr[i];
+        j = i - 1; 
+   
+        
+        while (j >= 0 && arr[j] > key)
+        { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
+    } 
+console.log(arr);
+
+
+   
+
